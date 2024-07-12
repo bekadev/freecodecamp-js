4a4567af4aa70f2826bf16e323e22643153643c0
Bekzat Kyzyrbayev <bekapng@gmail.com>
2024-07-12 17:22:38 +0500

    step-1

--- step-1.js
+++ step-1.js
@@ -61,6 +61,10 @@
 // const count = 8;
 // const rows = [];
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(i)
-}
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(i)
+// }
+//
+// for (const row of rows) {
+//
+// }
