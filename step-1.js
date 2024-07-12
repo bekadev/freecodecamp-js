004a2e003d52cef57ccda2876c941fa73d4eed03
Bekzat Kyzyrbayev <bekapng@gmail.com>
2024-07-12 14:24:23 +0500

    step-1

--- step-1.js
+++ step-1.js
@@ -1,5 +1,11 @@
-let character = 'Hello World!!';
-console.log(character);
+// let character = 'Hello World!!';
+// console.log(character);
+//
+// const dom = 1 + 2
+// console.log(dom)
+//
 
-const dom = 1 + 2
-console.log(dom)
+let character = 'Hello';
+console.log(character);
+character = "World";
+console.log(character);
