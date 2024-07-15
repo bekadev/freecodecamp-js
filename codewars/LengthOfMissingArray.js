dfc7f446260c6fcb002f0f4b7166c8a21ca3797e
Bekzat Kyzyrbayev <bekapng@gmail.com>
2024-07-15 22:34:13 +0500

    LengthOfMissingArray

--- codewars/LengthOfMissingArray.js
+++ codewars/LengthOfMissingArray.js
@@ -1,9 +1,11 @@
 // https://www.codewars.com/kata/57b6f5aadb5b3d0ae3000611/javascript
 
 function getLengthOfMissingArray(arrayOfArrays) {
-  let res = arrayOfArrays.map(e => e.length)
+  let res = (arrayOfArrays || []).map(e =>e ? e.length : 0)
     .sort((a, b) => a - b )
-
+  if(res.includes(0)){
+    return 0
+  }
   for (let i = 0; i < res.length; i++){
     if(res[i + 1] != res[i] + 1){
       return res[i] + 1
