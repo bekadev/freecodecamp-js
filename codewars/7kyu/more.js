c1c664f9eb2b4e7a63ca5419c57391ff0d79459e
Bekzat Kyzyrbayev <bekapng@gmail.com>
2024-07-29 21:22:27 +0500

    codewars js

--- codewars/7kyu/more.js
+++ codewars/7kyu/more.js
@@ -58,3 +58,16 @@ function getDivisorsCnt(n){
 
   return res
 }
+
+// https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
+
+function countSheeps(sheep) {
+  let res = 0
+
+  for (let i = 0; i < sheep.length; i++){
+    if(sheep[i] === true){
+      res++
+    }
+  }
+  return res
+}
