ef546d0c80258d1f202f4b757bc1f06ff7958f81
Bekzat Kyzyrbayev <bekapng@gmail.com>
2024-07-29 21:27:48 +0500

    codewars js

--- codewars/7kyu/more.js
+++ codewars/7kyu/more.js
@@ -78,3 +78,11 @@ function countSheeps(sheep) {
 const stringToNumber = function(str){
   return parseInt(str)
 }
+
+
+// https://www.codewars.com/kata/582cb0224e56e068d800003c/train/javascript
+
+
+function litres(time) {
+  return Math.floor(time * 0.5)
+}
